33136c27bd4d314ff55fd4e3cbf90d2cf347315d
AGR55 <gonzalezreyesadriano@gmail.com>
2025-02-24 22:09:36 -0500

    cambios en las variables de entorno

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -121,7 +121,8 @@ export const authOptions: NextAuthOptions = {
           prompt: "consent",
           access_type: "offline",
           response_type: "code",
-          scope: "openid email profile https://www.googleapis.com/auth/userinfo.profile"
+          scope: "openid email profile https://www.googleapis.com/auth/userinfo.profile",
+          redirect_uri: `${process.env.NEXTAUTH_URL}api/auth/callback/google`
         }
       },
       client: {
